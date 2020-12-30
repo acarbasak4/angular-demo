a34c2fcec82ebb7d91f2dd0097589e72bbcf41f8
Başak Acar <acarbasak4@gmail.com>
2020-12-30 15:50:20 +0300

    import products

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -8,6 +8,66 @@ import { Component, OnInit } from '@angular/core';
 export class ProductComponent implements OnInit {
 
   constructor() { }
+  title = "Product List";
+  products: any[] = [
+    {
+      id: 1,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Asus ZenBook"
+    },
+    {
+      id: 2,
+      name: "Mouse",
+      price: 110,
+      categoryId: 2,
+      description: "LogiTech"
+    },
+    {
+      id: 1,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Asus ZenBook"
+    },
+    {
+      id: 2,
+      name: "Mouse",
+      price: 110,
+      categoryId: 2,
+      description: "LogiTech"
+    },
+    {
+      id: 1,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Asus ZenBook"
+    },
+    {
+      id: 2,
+      name: "Mouse",
+      price: 110,
+      categoryId: 2,
+      description: "LogiTech"
+    },
+    {
+      id: 1,
+      name: "Laptop",
+      price: 2500,
+      categoryId: 1,
+      description: "Asus ZenBook"
+    },
+    {
+      id: 2,
+      name: "Mouse",
+      price: 110,
+      categoryId: 2,
+      description: "LogiTech"
+    }
+  ]
+
 
   ngOnInit(): void {
   }
