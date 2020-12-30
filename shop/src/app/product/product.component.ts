0d7ff7ba13760a718b11e0e2b69ad54185162385
Başak Acar <acarbasak4@gmail.com>
2020-12-30 16:37:34 +0300

    products listed

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from './product';
 
 @Component({
   selector: 'app-product',
@@ -9,62 +10,70 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
   title = "Product List";
-  products: any[] = [
+  products: Product[] = [
     {
       id: 1,
       name: "Laptop",
       price: 2500,
       categoryId: 1,
-      description: "Asus ZenBook"
+      description: "Asus ZenBook",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 2,
       name: "Mouse",
       price: 110,
       categoryId: 2,
-      description: "LogiTech"
+      description: "LogiTech",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 1,
       name: "Laptop",
       price: 2500,
       categoryId: 1,
-      description: "Asus ZenBook"
+      description: "Asus ZenBook",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 2,
       name: "Mouse",
       price: 110,
       categoryId: 2,
-      description: "LogiTech"
+      description: "LogiTech",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 1,
       name: "Laptop",
       price: 2500,
       categoryId: 1,
-      description: "Asus ZenBook"
+      description: "Asus ZenBook",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 2,
       name: "Mouse",
       price: 110,
       categoryId: 2,
-      description: "LogiTech"
+      description: "LogiTech",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 1,
       name: "Laptop",
       price: 2500,
       categoryId: 1,
-      description: "Asus ZenBook"
+      description: "Asus ZenBook",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     },
     {
       id: 2,
       name: "Mouse",
       price: 110,
       categoryId: 2,
-      description: "LogiTech"
+      description: "LogiTech",
+      imageUrl:"https://images.unsplash.com/photo-1533740566848-5f7d3e04e3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80"
     }
   ]
 
