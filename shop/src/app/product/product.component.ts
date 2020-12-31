f5e2c207632110f806834dbb763395012caf3340
Başak Acar <acarbasak4@gmail.com>
2020-12-31 15:14:56 +0300

    error handling

--- shop/src/app/product/product.component.ts
+++ shop/src/app/product/product.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product';
 import { AlertifyService } from '../services/alertify.service';
-import { HttpClient } from '@angular/common/http';
+import { ProductService } from '../services/product.service';
+
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
+  providers: [ProductService]
 })
 export class ProductComponent implements OnInit {
 
 
-  constructor(private alertifyService: AlertifyService, private http: HttpClient) { }
-  products: Product[];
-
+  constructor(private alertifyService: AlertifyService, private ProductService: ProductService) { }
   title = "Product List";
   filterText = "";
-  path = "http://localhost:3000/products"
-
+  products: Product[];
 
   ngOnInit() {
-    this.http.get<Product[]>(this.path).subscribe(data => {
+    this.ProductService.getProducts().subscribe(data => {
       this.products = data;
-    });
+    })
+
   }
 
   addToCart(product: { name: string; }) {
